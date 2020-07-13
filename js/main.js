3dd677a8fa81b9be4b8761f22d83bc8bbb7d453f
UmberBn <humberto-bandeira@hotmail.com>
2020-07-13 17:25:19 -0400

    Moving the Snake :)

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 function addBG(){
     context.fillStyle = "lightgreen";//background collor
     context.fillRect(0, 0 , 16 * box, 16*box);
@@ -19,5 +21,25 @@ function addSnake(){
 
 }
 
-addBG();
-addSnake();
+function startGame(){
+    addBG();
+    addSnake();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+    
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame,100);
+
