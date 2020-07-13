d355e52725357b8dd68b91c247f52f87498a766d
UmberBn <humberto-bandeira@hotmail.com>
2020-07-13 17:13:06 -0400

    Function that creating the snake

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
-
+let box = 32; //pixels
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 function addBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightgreen";//background collor
     context.fillRect(0, 0 , 16 * box, 16*box);
 }
 
-addBG()
+function addSnake(){
+    for(let i=0 ; i<snake.length; i++){
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x,snake[i].y,box,box);
+    }
+
+}
+
+addBG();
+addSnake();
