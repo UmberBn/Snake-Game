bf601eb6c8b6fc20f75691a90ed7e390f2dfdb8e
UmberBn <humberto-bandeira@hotmail.com>
2020-07-13 23:09:18 -0400

    Add the snake body

--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@ function drawFood(){
 document.addEventListener('keydown', update);
 
 function update (event){
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if((event.keyCode == 37 || event.keyCode == 65) && direction != "right") direction = "left";
+    if((event.keyCode == 38 || event.keyCode == 87) && direction != "down") direction = "up";
+    if((event.keyCode == 39 || event.keyCode == 68) && direction != "left") direction = "right";
+    if((event.keyCode == 40 || event.keyCode == 83) && direction != "up") direction = "down";
 }
 
 function startGame(){
@@ -54,9 +54,12 @@ function startGame(){
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else{  food.x = Math.floor(Math.random()* 15 + 1) * box;
+        food.y = Math.floor(Math.random()* 15 + 1) * box;}
 
-    snake.pop();
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
